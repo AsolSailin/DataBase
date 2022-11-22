d1ccd7b2211fc8eb91b214b82f0592c0115637cb
Ruslana <garaeva-r_23@mail.ru>
2022-11-23 00:51:06 +0300

    Add code

---
+++ Authorization/Authorization/Windows/RegistrationWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using Authorization.ADO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Authorization.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для RegistrationWindow.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Reg_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbLogin.Text != "" && tbPassword.Text != "" && tbName.Text != "" && tbRole.Text != "")
+            {
+                User newUser = new User()
+                {
+                    Name = tbName.Text,
+                    Role_Id = Convert.ToInt32(tbRole.Text)
+                };
+                Account newAccount = new Account()
+                {
+                    Login = tbLogin.Text,
+                    Password = tbPassword.Text
+                };
+
+                newUser.Account.Add(newAccount);
+                App.Connection.User.Add(newUser);
+                App.Connection.Account.Add(newAccount);
+                App.Connection.SaveChanges();
+                MessageBox.Show("Registration is successfull");
+            }
+            else
+            {
+                MessageBox.Show("Incorrect data");
+            }
+        }
+    }
+}
